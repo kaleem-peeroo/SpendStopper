45eb9a6affe19f664e4d6035c1095d9ca82daacc
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2025-11-23 21:56:09 +0000

    Added parameter checks for encrypt_file()

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,5 @@
-# encrypt.py
 from cryptography.fernet import Fernet
+import os
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -16,7 +16,16 @@ def get_key_from_password(password: str, salt: bytes) -> bytes:
     return key
 
 
-def encrypt_file(input_file, output_file, password):
+def encrypt_file(input_file: str = "", output_file: str = "", password: str = ""):
+    if input_file == "" or input_file is None:
+        raise ValueError("Empty input file")
+
+    if output_file == "" or output_file is None:
+        raise ValueError("Empty output file")
+
+    if password == "" or password is None:
+        raise ValueError("Empty password")
+
     salt = os.urandom(16)
     key = get_key_from_password(password, salt)
     fernet = Fernet(key)
